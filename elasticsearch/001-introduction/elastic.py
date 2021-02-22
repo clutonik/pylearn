7ffac2b352664a46996326ef57d6a33b77e421f5
Sahil Chawla <sahil.chawla@clutonik.ca>
2021-02-22 02:15:09 -0500

    Changes

--- elasticsearch/001-introduction/elastic.py
+++ elasticsearch/001-introduction/elastic.py
@@ -1,19 +1,33 @@
 # Code to work with elasticsearch python library
 from elasticsearch import Elasticsearch
 
+
+def create_index(*args, **kwargs):
+    print(args)
+    print(kwargs)
+    #self.index_name = name
+    #self.index_mapping_body = body
+
+
+def create_mapping_body(**kwargs):
+    mapping_body = {"mappings": {"properties": {}}}
+    for field in kwargs:
+        if field not in mapping_body["mappings"]["properties"].keys():
+            mapping_body["mappings"]["properties"][field] = {
+                "type": kwargs.get(field)
+            }
+
+    return mapping_body
+
+
 if __name__ == '__main__':
-    print('Hello World')
     es = Elasticsearch()
-    index_mapping_body = {
-       "mappings": {
-           "properties": {
-               "age":    { "type": "integer" },
-               "email":  { "type": "keyword"  },
-               "name":   { "type": "text"  }
-           }
-       }
-    }
-    create_response = es.indices.create(index='test-index-with-mapping',body=index_mapping_body)
+
+    mapping_body = create_mapping_body(age="integer",
+                                       email="keyword",
+                                       name="text")
+
+    create_response = es.indices.create(index='test-index-with-mapping-2',
+                                        body=mapping_body,
+                                        ignore=400)
     print(create_response)
-    response = es.indices.exists(index='test-index')
-    print(response)
