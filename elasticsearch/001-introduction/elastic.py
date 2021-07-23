9036357bbcb126d18567108b9da616985d1b3d75
Sahil Chawla <sahil.chawla@clutonik.ca>
2021-07-22 20:20:12 -0400

    fastapi base

---
+++ elasticsearch/001-introduction/elastic.py
@@ -0,0 +1,37 @@
+# Code to work with elasticsearch python library
+from elasticsearch import Elasticsearch
+
+
+def insert_data(name=None, doc=None, doc_id=None, **kwargs):
+    """Function to insert data!
+
+    :param name:
+    :param doc:
+    :param doc_id:
+    :returns:
+    :rtype:
+
+    """
+    pass
+
+
+def insert_row(name=None, doc=None, doc_id=None, **kwargs):
+    """Function to insert data!
+
+    :param name:
+    :param doc:
+    :param doc_id:
+    :returns:
+    :rtype:
+
+    """
+    pass
+
+
+if __name__ == '__main__':
+    print('Hello World')
+    a = 10
+    b = 20
+    # es = Elasticsearch()
+    insert_data()
+    insert_row()
