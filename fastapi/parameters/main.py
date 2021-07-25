60f2701c0d7b4c5e47f52fcf0b94ece067e6abdc
Sahil Chawla <sahil.chawla@clutonik.ca>
2021-07-24 22:14:46 -0400

    fastapi query parameters

--- fastapi/parameters/main.py
+++ fastapi/parameters/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -19,7 +20,31 @@ async def read_products(skip: int = 0, limit: int = 100):
 # ---------------
 # QUERY Parameters (required to be defined)
 # ---------------
-@app.get("/items/")
+@app.get("/items/required")
 # NOTE: skip is a required parameter, but limit is not
 async def read_products(skip: int, limit: int = 100):
     return {"skip": skip, "limit": limit}
+
+# ---------------
+# QUERY Parameters (optional)
+# ---------------
+# NOTE: q is an optional parameter and its default value is None
+@app.get("/items/optional")
+async def read_optional_items(skip: int = 0, q: Optional[str] = None, limit: int = 100):
+    if q:
+        return {"skip": skip, "limit": limit, "q": q}
+    return {"skip": skip, "limit": limit}
+
+# ---------------
+# QUERY Parameters (booleans)
+# ---------------
+# Note: You can use bool query parameters using below ways:
+# 1. http://localhost:8000/items/booleans?all=False
+# 2. http://localhost:8000/items/booleans?all=0
+# 3. http://localhost:8000/items/booleans?all=yes
+@app.get("/items/booleans")
+async def read_items_booleans(skip: int = 0, all: bool = True):
+    if all:
+        return {"products": "all"}
+
+    return {"products": "some"}
